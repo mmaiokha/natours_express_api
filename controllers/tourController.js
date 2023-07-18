91357a6931ea30a34f997cf0d45cf3a2e73ff7e7
Glipher G <imnotsmarttt@gmail.com>
2023-07-18 10:57:53 +0300

    feat: tour stat & tour year plan

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,13 +1,6 @@
 const Tour = require('../models/tourModel')
 const APIFeatures = require('../utils/apiFeatures')
 
-exports.topFiveToursMiddleware = (req, res, next) => {
-    req.query.limit = '5'
-    req.query.sort = '-ratingsAverage'
-    req.query.fields = 'name,price,ratingsAverage,summary,difficulty,'
-    next()
-}
-
 exports.createTour = async (req, res) => {
     try {
         const newTour = await Tour.create(req.body)
@@ -89,6 +82,96 @@ exports.deleteTour = async (req, res) => {
         return res.status(200).json({
             status: 'success',
             message: 'Tour successfully deleted'
+        })
+    } catch (e) {
+        return res.status(400).json({
+            status: 'fail',
+            message: e
+        })
+    }
+}
+
+exports.topFiveToursMiddleware = (req, res, next) => {
+    req.query.limit = '5'
+    req.query.sort = '-ratingsAverage'
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty,'
+    next()
+}
+
+exports.getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([
+            {
+                $group: {
+                    _id: {$toUpper: '$difficulty'},
+                    numTours: {$sum: 1},
+                    numRating: {$sum: '$ratingsQuantity'},
+                    avgPrice: {$avg: '$price'},
+                    avgRating: {$avg: '$ratingsAverage'},
+                    minPrice: {$min: '$price'},
+                    maxPrice: {$max: '$price'},
+                }
+            },
+            {
+                $addFields: {difficulty: '$_id'}
+            },
+            {
+                $project: {_id: 0}
+            }
+        ])
+        return res.status(201).json({
+            status: 'success',
+            result: stats.length,
+            data: {
+                stats
+            }
+        })
+    } catch (e) {
+        return res.status(400).json({
+            status: 'fail',
+            message: e
+        })
+    }
+}
+
+exports.getMonthlyPlan = async (req, res) => {
+    try {
+        const year = req.params.year
+        const plan = await Tour.aggregate([
+            {
+                $unwind: '$startDates'
+            },
+            {
+                $match: {
+                    startDates: {
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-01`),
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: {$month: '$startDates'},
+                    tourSum: {$sum: 1},
+                    tours: {$push: '$name'}
+                }
+            },
+            {
+                $sort: {_id: 1}
+            },
+            {
+                $addFields: {month: '$_id'}
+            },
+            {
+                $project: {'_id': 0}
+            }
+        ])
+        return res.status(201).json({
+            status: 'success',
+            result: plan.length,
+            data: {
+                plan
+            }
         })
     } catch (e) {
         return res.status(400).json({
--- routes/toursRouter.js
+++ routes/toursRouter.js
@@ -4,6 +4,8 @@ const tourController = require('./../controllers/tourController')
 const tourRouter = express.Router()
 
 tourRouter.route('/top-5').get(tourController.topFiveToursMiddleware, tourController.getAllTours)
+tourRouter.route('/stats').get(tourController.getTourStats)
+tourRouter.route('/plan/:year').get(tourController.getMonthlyPlan)
 
 tourRouter
     .route('/')
